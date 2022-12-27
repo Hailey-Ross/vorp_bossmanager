14f411b1f5a224b51fd31280508f6b3ed47215f6
Hailey <26784904+Hailey-Ross@users.noreply.github.com>
2022-12-27 09:41:32 -0700

    Creation

---
+++ en.lua
@@ -0,0 +1,12 @@
+Locales['en'] = {
+    ['title'] = 'Boss',
+    ['subtitle'] = 'Boss Menu',
+    ['subtitle2'] = 'Employee Name',
+    ['subtitle3'] = 'Employee List',
+    ['sub_menu1'] = 'Hire',
+    ['sub_menu2'] = 'Dismiss',
+    ['gradelbl'] = 'Grade',
+    ['inputgrademsg'] = 'Enter a Rank',
+    ['gradeerrormsg'] = 'Invalid Rank',
+    ['error_noclosestplayer'] = 'No one Nearby',
+}
